124d69cd4f495ad4231630d0e50500492101d66e
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-16 18:49:17 +0200

    add title component

---
+++ hangman-app/src/components/Title.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+// import PropTypes from 'prop-types'
+
+export default class Title extends PureComponent {
+  
+  
+  render() {
+    return <h1>Welcome to the Hangman!</h1>
+  }
+}
