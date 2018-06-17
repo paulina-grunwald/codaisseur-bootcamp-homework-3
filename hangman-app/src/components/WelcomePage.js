4cd3ec5801f62dc868476bbd2ff05c1c104a3f9a
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-17 18:40:10 +0200

    change welcome message as per assigment requirment

--- hangman-app/src/components/WelcomePage.js
+++ hangman-app/src/components/WelcomePage.js
@@ -5,7 +5,7 @@ class WelcomePage extends React.Component {
   render() {
     return (
       <div>
-        <Link to={`/hangman/`}><h1>Go to hangman</h1></Link>
+        <Link to={`/hangman/`}><h1>Start playing</h1></Link>
       </div>
     );
   }
