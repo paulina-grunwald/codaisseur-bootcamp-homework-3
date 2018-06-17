0b5b6d87aa99718e9b33d0135b290b0833301172
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-17 18:07:54 +0200

    add redux-thunk and delete old logo

--- hangman-app/src/store.js
+++ hangman-app/src/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
+import ReduxThunk from 'redux-thunk'
 
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
+const enhancer = compose(applyMiddleware(ReduxThunk),devTools)
 const store = createStore(reducer, enhancer)
 
 export default store
