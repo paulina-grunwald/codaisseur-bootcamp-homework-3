9cb7adca5ffd51d8983141871fd4177b6a8cafaa
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-17 15:12:15 +0200

    update reducers

--- hangman-app/src/reducers/guesses.js
+++ hangman-app/src/reducers/guesses.js
@@ -4,7 +4,7 @@ export const NEW_GAME = 'NEW_GAME'
 export default function(state = [], action) {
   switch (action.type) {
   case NEW_GAME:
-    return state = []
+    return state
   case MAKE_GUESS:
     return [...state, action.payload.guessedLetter]
   default:
--- hangman-app/src/reducers/secretWord.js
+++ hangman-app/src/reducers/secretWord.js
@@ -1,9 +1,12 @@
+import {randomWord} from '../lib/game'
+
 export const NEW_GAME = 'NEW_GAME'
 
-export default function(state = '', action) {
+
+export default function(state = randomWord(), action) {
   switch (action.type) {
   case NEW_GAME:
-    return action.payload
+    return action.payload.word
   default:
     return state
   }
